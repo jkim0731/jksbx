ef52c09b70aeb13a2b16a579f0f724b97a773f60
Jinho Kim <jkimkj@gmail.com>
2018-10-07 13:39:03 -0700

    dF update

--- datdFcorrection.m
+++ datdFcorrection.m
@@ -1,27 +1,33 @@
 clear
-mice = [25,27,30,36,37,39,52,53,54,56];
-sessions = {[4,19],[3,16],[3,21],[1,17],[7],[1,22],[3,21],[3],[3],[3]};  
+mice = [36,37,39,52,53,54,56];
+sessions = {[1,17],[7],[1,22],[3,21],[3],[3],[3]};  
 rollingWindowForBaseF = 100; % in s
 baseFprctile = 5;
-baseDir = 'C:\Data\suite2p\';
+baseDir = 'D:\TPM\JK\suite2p\';
 for mi = 1 : length(mice)
     cd([baseDir, sprintf('%03d',mice(mi))])
     fList = dir('F_*_proc_final.mat');
     for i = 1 : length(fList)
+        fprintf('mouse %03d session %d\n', mice(mi), i)
         load(fList(i).name)
         dat.rollingWindowForBaseF = rollingWindowForBaseF;
         dat.baseFprctile = baseFprctile;
-        dat.dF = zeros(size(dat.Fcell{1}), 'like', dat.Fcell{1});
-        for j = 1 : size(dat.Fcell{1},1)
-            tempF = dat.Fcell{1}(j,:) - dat.FcellNeu{1}(j,:) * dat.stat(j).neuropilCoefficient;
-            window = round(dat.rollingWindowForBaseF*(dat.ops.imageRate/dat.ops.num_plane));
-            tempFF = [tempF, tempF(end-window+1:end)];
-            baseF = zeros(size(tempF));
-            for k = 1 : length(baseF)
-                baseF(k) = prctile(tempFF(k:k+window),dat.baseFprctile);
+        window = round(dat.rollingWindowForBaseF*(dat.ops.imageRate/dat.ops.num_plane));
+        dF = zeros(size(dat.Fcell{1}), 'like', dat.Fcell{1});
+        parfor j = 1 : size(dat.Fcell{1},1)
+            if (dat.stat(j).iscell)
+                fprintf('cell #%d/%d\n',j,size(dat.Fcell{1},1))
+                tempF = dat.Fcell{1}(j,:) - dat.FcellNeu{1}(j,:) * dat.stat(j).neuropilCoefficient;
+                window = round(dat.rollingWindowForBaseF*(dat.ops.imageRate/dat.ops.num_plane));
+                tempFF = [tempF, tempF(end-window+1:end)];
+                baseF = zeros(size(tempF));
+                for k = 1 : length(baseF)
+                    baseF(k) = prctile(tempFF(k:k+window),dat.baseFprctile);
+                end
+                dF(j,:) = (tempF-baseF)./baseF;
             end
-            dat.dF(j,:) = (tempF-baseF)./baseF;
         end
+        dat.dF = dF;
         save(fList(i).name, 'dat')
     end
 end
--- jk_postprocess.m
+++ jk_postprocess.m
@@ -1,18 +1,8 @@
-%% Split the image into each trials
-mice = {'052','053','054','056'};
-base_dir = 'D:\2p\JK';
-for i = 1 : length(mice)
-    cd([base_dir, filesep, mice{i}])   
-    jksbxsplittrial_dir
-end
+%% First, make db from jk_make_db, then run jk_master_file (suite2p)
+
+%% then, process z-stack and C2
+
+%% then, finally, sort active cells and make dF/F0
 
 
-%% Alignment
-mice = {'052','053','054','056'};
-base_dir = 'D:\2p\JK';
-
-for i = 1 : size(mice,2)    
-    cd([base_dir, filesep, mice{i}])
-    jksbxaligndir({},'no','skip')
-end
 
